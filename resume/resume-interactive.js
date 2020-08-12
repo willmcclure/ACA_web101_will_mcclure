7dc908294c2efb6e3138d498291e035d85084203
@willmcclure <willbmcclure@gmail.com>
2020-08-11 23:25:01 -0500

    Resume interactivity updated

---
+++ resume/resume-interactive.js
@@ -0,0 +1,11 @@
+function fadeout(title) {
+    title.style.opacity = '0';
+  };
+
+  function disappear(img) {
+    img.style.display = 'none';
+  };
+
+  function appear(phone) {
+    phone.style.opacity = '1';
+  }
