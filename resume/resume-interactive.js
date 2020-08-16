20eedb3a0c38528e5a7ea76e024951498deb79b0
@willmcclure <willbmcclure@gmail.com>
2020-08-16 14:09:01 -0500

    Homework complete

--- resume/resume-interactive.js
+++ resume/resume-interactive.js
@@ -1,11 +1,21 @@
 function fadeout(title) {
-    title.style.opacity = '0';
-  };
-
-  function disappear(img) {
-    img.style.display = 'none';
-  };
-
-  function appear(phone) {
-    phone.style.opacity = '1';
+  if (title.style.opacity === "0") {
+    title.style.opacity = "1";
+  } else {
+    title.style.opacity = "0";
   }
+}
+
+function disappear(img) {
+  if (img.style.opacity === '0') {
+    img.style.opacity = '1'
+  } else {img.style.opacity = '0';
+  }
+};
+
+function appear(phone) {
+  if (phone.style.opacity === "0") {
+    phone.style.opacity = "1";
+  } else { phone.style.opacity = "0";
+  }
+ }
